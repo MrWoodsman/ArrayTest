ea11f450c89036df099d8ccfed3c388cb0d13b20
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2022-12-21 00:21:20 +0100

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,8 @@
       {'id':'2','title':'Wigilia','date':'24.12.2022','blank':false,'img':'https://st.depositphotos.com/2121227/3001/i/600/depositphotos_30011609-stock-photo-gingerbread-cookies-on-christmas-eve.jpg'},
       {'id':'3','title':'Sylwester','date':'31.12.2022','blank':false,'img':'https://i.pinimg.com/originals/3d/07/8b/3d078b8f5fc684c8b0e3691fbeaadccf.jpg'},
       {'id':'4','title':'Nowy Rok','date':'01.01.2023','blank':false,'img':'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/happy-new-year-2023-design-template-910d001f2c66a6aa87cde05e4646f734_screen.jpg?ts=1667647084'},
-      {'id':'5','title':'BLANK','date':'00.00.0000','blank':true,'img':''},
+      {'id':'5','title':'Testowe Wydarzenie','date':'15.01.2023','blank':false,'img':''},
+      {'id':'9999','title':'BLANK','date':'00.00.0000','blank':true,'img':''},
     ]
 
     var freeDays = [
